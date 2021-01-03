66f499f36220dd113a69b8c1241c8de551bf5b9b
Michelle Gonzalez <mixesthe@gmail.com>
2021-01-02 19:19:05 -0500

    finished assigment-xx

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,11 @@
 function titleCaseEdit(title) {
   // Insert code here;
+  let FixingTittle = title.toLowerCase();
+  FixingTittle = FixingTittle.split(" ");
+  for(let i = 0; i <FixingTittle.length;i++){
+    FixingTittle[i] = FixingTittle[i].charAt(0).toUpperCase() + FixingTittle[i].slice(1);
+  }
+  return FixingTittle.join(" ");
 }
 
 // Do not edit this line;
